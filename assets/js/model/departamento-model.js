a5d5bf11f3ed696232bbb82ca20451c5ae55f954
gabguedes <gabriel4g.crispim@gmail.com>
2024-09-28 23:08:10 -0300

    wip: listagem todos os treinamentos, listagem de usuarios e treinadores, e insert de treinadores

---
+++ assets/js/model/departamento-model.js
@@ -0,0 +1,8 @@
+export default class Departamento{
+    constructor(id, nome, funcionarios, treinamentos){
+        this.id = id,
+        this.nome = nome,
+        this.funcionarios = funcionarios,
+        this.treinamentos = treinamentos
+    }
+}
---
+++ assets/js/model/lista-de-presenca-model.js
@@ -0,0 +1,9 @@
+class ListaDePresenca{
+    constructor(id, data_criacao, codigo, treinamento, funcionarios){
+        this.id = id,
+        this.data_criacao = data_criacao,
+        this.codigo = codigo,
+        this.treinamento = treinamento,
+        this.funcionarios = funcionarios
+    }
+}
